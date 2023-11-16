b406c3c7531e4ee9bcfbebdb32619d87716fdd23
GilangRimbawan <63135777+GilangRimbawan@users.noreply.github.com>
2023-11-16 22:17:43 +0700

    Update SepatuFormal.java

--- SepatuFormal.java
+++ SepatuFormal.java
@@ -1,12 +1,14 @@
-// File: SepatuFormal.java
 public class SepatuFormal extends Sepatu {
+    // Field final tambahan
     final String warna;
 
+    // Constructor dengan super untuk memanggil constructor dari superclass
     public SepatuFormal(String merek, int ukuran, String warna) {
         super(merek, ukuran);
         this.warna = warna;
     }
 
+    // Overriding method deskripsi
     @Override
     public void deskripsi() {
         System.out.println("Ini adalah sepatu formal merek " + merek + " dengan ukuran " + ukuran + " dan warna " + warna);
