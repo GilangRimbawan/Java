8a657badc72acd222b93efe3e316209c26340fd1
GilangRimbawan <63135777+GilangRimbawan@users.noreply.github.com>
2023-11-16 22:17:11 +0700

    Update Sepatu.java

--- Sepatu.java
+++ Sepatu.java
@@ -1,13 +1,15 @@
-// File: Sepatu.java
 public class Sepatu {
+    // Field final
     final String merek;
     final int ukuran;
 
+    // Constructor
     public Sepatu(String merek, int ukuran) {
         this.merek = merek;
         this.ukuran = ukuran;
     }
 
+    // Method yang akan di-overriden oleh subclass
     public void deskripsi() {
         System.out.println("Ini adalah sepatu merek " + merek + " dengan ukuran " + ukuran);
     }
